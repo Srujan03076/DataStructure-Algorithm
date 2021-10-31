be2b745b750c1f8ffea0ed5eaffef9e9fab5a193
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 14:06:13 +0530

    Added Bubble Sort

---
+++ BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Bubblesort
+
+    {
+        public void BubbleSort()
+        {
+            int[] numbers = { 43, 70, 20, 77, 14 };
+            int temp;
+            for (int i = 0; i <= numbers.Length - 2; i++)
+            {
+                for (int j = 0; j <= numbers.Length - 2; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        temp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            foreach (int p in numbers)
+                Console.Write($"{p}, ");
+        }
+    }
+}
