6171b40511ecd9fadd569978651cc6bc344d1de7
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 13:32:01 +0530

    Added Insertion Sort

---
+++ InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class DataStructure
+    {
+        public void Insertion(IComparable[] names)
+        {
+            int i, j;
+
+            for (i = 1; i < names.Length; i++)
+            {
+                IComparable data = names[i];
+                j = i - 1;
+                while ((j >= 0) && (names[j].CompareTo(data) > 0))
+                {
+                    names[j + 1] = names[j];
+                    j--;
+                }
+                names[j + 1] = data;
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,20 +6,25 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-           
+           //BinarySearchWord
 
-            Console.WriteLine("Data Structure!");
-            BinarySearchword dataStructure = new BinarySearchword();
-            string[] words = { "decs", "read", "logic", "search" };
-            string data = "search";
-            int result = dataStructure.FindWord(words, data);
-            if (result == -1)
-                Console.WriteLine("Word not present");
+            //Console.WriteLine("Data Structure!");
+            //BinarySearchword dataStructure = new BinarySearchword();
+            //string[] words = { "decs", "read", "logic", "search" };
+            //string data = "search";
+            //int result = dataStructure.FindWord(words, data);
+            //if (result == -1)
+            //    Console.WriteLine("Word not present");
 
-            else
-                Console.WriteLine("Word is found at " + result);
+            //else
+            //    Console.WriteLine("Word is found at " + result);
 
+            //InsertionSort
 
+             Console.WriteLine("Data Structure!");
+            string[] names = { "One", "Two", "Four", "Six" };
+            DataStructure dataStructure = new DataStructure();
+            dataStructure.Insertion(names);
 
         }
     }
