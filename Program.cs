83067cc4c86582da2da899721b7e468f420d77bb
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 13:37:09 +0530

    Added Insertion Sort

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-           //BinarySearchWord
+       //BinarySearchWord
 
             //Console.WriteLine("Data Structure!");
             //BinarySearchword dataStructure = new BinarySearchword();
@@ -19,7 +19,7 @@ namespace DataStructures
             //else
             //    Console.WriteLine("Word is found at " + result);
 
-            //InsertionSort
+        //InsertionSort
 
              Console.WriteLine("Data Structure!");
             string[] names = { "One", "Two", "Four", "Six" };
