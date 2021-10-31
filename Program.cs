eee7961b1be78e9d04e7a76a7aff69aec9762b5c
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 13:24:20 +0530

    Added Binary Search Word

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Data Structure!");
+            BinarySearchword dataStructure = new BinarySearchword();
+            string[] words = { "decs", "read", "logic", "search" };
+            string data = "search";
+            int result = dataStructure.FindWord(words, data);
+            if (result == -1)
+                Console.WriteLine("Word not present");
+
+            else
+                Console.WriteLine("Word is found at " + result);
+
+
+
+        }
+    }
+}
+
+
+
+
+  
