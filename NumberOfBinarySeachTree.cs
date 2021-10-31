e01c07ee418774a66f3b521e63381fee4fff3896
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 13:58:55 +0530

    Added Number of Binary Search Tree

---
+++ NumberOfBinarySeachTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DataStructures
+{
+    class DataStructure<Gtype>
+    {
+        public void Prime()
+        {
+            List<int> primes = new List<int>();
+            int count = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                int flag = 0;
+                for (int j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    primes.Add(i);
+                    count++;
+                }
+            }
+            foreach (int i in primes)
+            {
+                Console.Write($"{i}, ");
+            }
+        }
+    }
+}
