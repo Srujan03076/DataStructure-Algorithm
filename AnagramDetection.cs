7d8b8b324bc45a243f789b1c7795eccd4e556c78
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 13:48:31 +0530

    Added Anagram Detection

---
+++ AnagramDetection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class DataStructurealgorithm
+    {
+        public void Anagrams()
+        {
+            string str1 = "heart";
+            string str2 = "earth";
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string first = new string(ch1);
+            string second = new string(ch2);
+            if (first == second)
+                Console.WriteLine("Strings are Anagrams");
+            else
+                Console.WriteLine("Strings are not Anagrams");
+        }
+    }
+}
